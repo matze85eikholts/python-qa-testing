68d911d3b4ac73f2cd43cc1138612e73f7bd911c
Matvey Eikholts <matze85eikholts@gmail.com>
2023-11-08 07:18:03 +0300

    tests with assert added

---
+++ tests/test_examples.py
@@ -0,0 +1,9 @@
+class TestExample:
+    def test_check_math_1(self):
+        a = 4
+        b = 8
+        assert a + b == 12
+    def test_check_math_2(self):
+        a = 6
+        b = 14
+        assert a + b == 12
